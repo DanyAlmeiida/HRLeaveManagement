c4b97c942cb780383c795082d9fb0becb2b14179
Dany Almeida <dany_almeida@outlook.pt>
2022-09-21 16:28:41 +0100

    initial commit

---
+++ HRLeaveManagement.Domain/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HRLeaveManagement.Domain
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
