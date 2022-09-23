53337e0cc8a2aa971d4eaabd74cbcc496f34e2ea
Dany Almeida <dany_almeida@outlook.pt>
2022-09-23 01:43:23 +0100

    Commands & Handlers to allow delete!

---
+++ HRLeaveManagement.Application/Features/LeaveAllocations/Handlers/Commands/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
+using HRLeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveAllocations.Handlers.Commands
+{
+    public class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand>
+    {
+        private readonly ILeaveAllocationRepository repository;
+        private readonly IMapper mapper;
+
+        public DeleteLeaveAllocationCommandHandler(ILeaveAllocationRepository repository,IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await repository.Get(request.Id);
+            await repository.Delete(leaveAllocation);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HRLeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HRLeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class DeleteLeaveRequestCommandHandler : IRequestHandler<DeleteLeaveRequestCommand>
+    {
+        private readonly ILeaveRequestRepository repository;
+        private readonly IMapper mapper;
+
+        public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository repository,IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await repository.Get(request.Id);
+            await repository.Delete(leaveRequest);
+
+            return Unit.Value;
+        }
+    }
+}
