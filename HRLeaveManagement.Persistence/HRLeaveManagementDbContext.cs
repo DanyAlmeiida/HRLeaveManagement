6bff76d1063227c868bbb35b38b12a60335e2647
Dany Almeida <dany_almeida@outlook.pt>
2022-09-26 20:42:37 +0100

    Persistence Layer - Entity Framework - Repo Definition

---
+++ HRLeaveManagement.Persistence/HRLeaveManagementDbContext.cs
@@ -0,0 +1,42 @@
+﻿using HRLeaveManagement.Domain;
+using HRLeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence
+{
+    public class HRLeaveManagementDbContext : DbContext
+    {
+        public HRLeaveManagementDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+        //when db is being generate
+        //set rules
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRLeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
---
+++ HRLeaveManagement.Persistence/repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using HRLeaveManagement.Application.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence.repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly HRLeaveManagementDbContext dbContext;
+
+        public GenericRepository(HRLeaveManagementDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<T> Add(T entity)
+        {
+            await dbContext.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task Delete(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await Get(id);
+            return entity != null;
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await dbContext.Set<T>().FindAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAll()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
